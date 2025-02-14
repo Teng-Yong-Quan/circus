0ad60c833311827a30b68bebf3c016d06d756ab6
Teng-Yong-Quan <tengyongquan5ki@gmail.com>
2025-02-14 17:25:07 +0800

    Commit new changes

--- src/main/java/circus/animal/Animal.java
+++ src/main/java/circus/animal/Animal.java
@@ -2,8 +2,11 @@ package circus.animal;
 
 import circus.Asset;
 
+import java.util.Comparator;
+
 public abstract class Animal implements Asset {
 
     public String name;
     public abstract String speak();
+
 }
